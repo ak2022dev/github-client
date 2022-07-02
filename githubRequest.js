ca310976842de9b4e64e47d0c9d5d9c5eb759fc7
Amir Khan <ak2022dev@gmail.com>
2022-07-02 09:25:26 +0100

    Complete initial exercise to make github request then format it

---
+++ githubRequest.js
@@ -0,0 +1,28 @@
+// file: githubRequest.js
+
+// Load the `got` function
+const got = require('got');
+
+/*
+// Create a "handler" callback function
+const handleReceivedResponse = (response) => {
+  console.log( response.body );
+}
+*/
+
+const url = 'https://api.github.com/repos/sinatra/sinatra';
+
+/*
+// Call `got` and provide the handler callback function.
+// This will get called by `got` when the response is received.
+got(url).then(handleReceivedResponse);
+*/
+
+// We can also rewrite the above using a shorter version,
+// by defining the "handler" function as an anonymous function
+
+got(url)
+  .then( (response) => {
+	  console.log( JSON.parse(response.body) );
+  });
+
